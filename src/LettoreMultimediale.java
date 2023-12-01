d2cc97114aa826d28aa3613a66a83efcc91a74b9
Vincenzo Melillo <137229406+Vince-mel@users.noreply.github.com>
2023-12-01 12:59:18 +0100

    completato show() immagine e test.

--- src/LettoreMultimediale.java
+++ src/LettoreMultimediale.java
@@ -2,7 +2,7 @@ import entities.Immagine;
 
 public class LettoreMultimediale {
     public static void main(String[] args) {
-        Immagine im1 = new Immagine("bau",5);
-        im1.show();
+        Immagine img1 = new Immagine("bau",5);
+        img1.show();
         }
     }
